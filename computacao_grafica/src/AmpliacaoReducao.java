a4039b30ffcf83b2ef67f805c36de5bfb6893438
neo-mateus <neomateus33@gmail.com>
2026-09-16 21:30:33 -0300

    Exercício: Ampliação e Redução de imagens

---
+++ computacao_grafica/src/AmpliacaoReducao.java
@@ -0,0 +1,55 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mateu
+ */
+public class AmpliacaoReducao extends JFrame {
+    
+    public static void main(String[] args) {
+        String img = "mini_einstein.jpg";
+        
+        //tipoOp 1 - ampliação, 2- redução
+        int fator = 1, tipoOp = 1;
+        
+        if(fator <= 0)
+            fator = 1;
+        if((tipoOp <= 0) || (tipoOp > 2))
+            tipoOp = 1;
+       
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
+        JFrame zoomImg = new AmpliacaoReducao(img, fator, tipoOp);
+        
+        zoomImg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        zoomImg.setVisible(true);
+    }
+    
+    public AmpliacaoReducao(String aFile, int aFator, int aTipoOp){
+        BufferedImage bimage = null, dest = null;
+        
+        File file = new File(aFile);
+        try{
+            bimage = ImageIO.read(file);
+        } catch (IOException e){
+            System.out.println("Imagem '" + aFile + "' nao existe.");
+            System.exit(0);
+        }
+        
+        
+        
+    }
+    
+    public BufferedImage zoomImage(BufferedImage image, int fator, int tipoOp){
+    }
+}
